5321bdf46ee74a15e63f19ca226cf43f5742661d
Natalie Martin <nmartin2473@gmail.com>
2025-10-03 19:06:18 -0500

    Fix typing for PHP 7
    We're targeting PHP 7 tehe

--- src/impl/FilesystemReleaseGenerator.php
+++ src/impl/FilesystemReleaseGenerator.php
@@ -7,7 +7,6 @@ require_once(__DIR__."/../types/ReleaseGenerator.php");
  */
 class FilesystemReleaseGenerator implements ReleaseGenerator {
   public function createDirectory(string $dirname): void {
-    echo $dirname . "\n";
     if (!empty($dirname) && !is_dir($dirname)) {
       if (!mkdir($dirname, 0777, true)) {
         $error = error_get_last();
--- src/types/ReleaseManager.php
+++ src/types/ReleaseManager.php
@@ -7,8 +7,8 @@ require_once(__DIR__."/Release.php");
  * Takes in a release and sends its deploy paths to a `ReleaseGenerator` to publish a new release.
  */
 class ReleaseManager {
-  public string $baseDirectory;
-  private ReleaseGenerator $generator;
+  public $baseDirectory;
+  private $generator;
 
   public function __construct(ReleaseGenerator $generator) {
     $this->generator = $generator;
@@ -17,14 +17,14 @@ class ReleaseManager {
   /**
    * Create the directories for a new release
    */
-  public function publishRelease(Release $release): void {
+  public function publishRelease(Release $release) {
     $paths = $release->getDeployPaths();
     foreach ($paths as $path) {
       $this->generator->createDirectory($this->getFullPath($path));
     }
   }
 
-  private function getFullPath(string $path): string {
+  private function getFullPath(string $path) {
     if (isset($this->baseDirectory)) {
       return $this->baseDirectory . "/" . $path;
     }
